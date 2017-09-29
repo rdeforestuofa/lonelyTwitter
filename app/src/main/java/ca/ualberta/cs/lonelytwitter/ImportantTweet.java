f7cfb48eef3342700c0d6d54f2a7c431249f1ac3
rdeforestuofa <deforest@ualberta.ca>
2017-09-28 19:15:49 -0600

    lab exercise 4

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
@@ -1,20 +1,51 @@
+/*
+ * Class Name: ImportantTweet
+ *
+ * Version: Version 1.0
+ *
+ * Date : September 28, 2017
+ *
+ * Copyright (c) My Copyright, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
+ */
+
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
+
 /**
- * Created by deforest on 9/14/17.
+ * Represents an ImportantTweet
+ *
+ * @author Ryan De Forest
+ * @version 1.0
+ * @since 1.0
  */
 
 public class ImportantTweet extends Tweet {
+    /**
+     * Constructs an ImportantTweet object with only a message
+     * @param message tweet message
+     */
+
     public ImportantTweet(String message){
         super(message);
     }
+
+    /**
+     * Constructs an ImportantTweet object with a message and date
+     * @param message tweet message
+     * @param date tweet date
+     */
 
     public ImportantTweet(String message, Date date) {
         super(message, date);
     }
 
+
+    /**
+     * Returns true for the overridden method as this is from an ImportantTweet
+     * @return true
+     */
     @Override
     public Boolean isImportant(){
         return Boolean.TRUE;
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Mood.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Mood.java
@@ -1,29 +1,70 @@
+/*
+ * Class Name: Mood
+ *
+ * Version: Version 1.0
+ *
+ * Date : September 28, 2017
+ *
+ * Copyright (c) My Copyright, CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Students Behavior at University of Alberta
+ */
+
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
 /**
- * Created by deforest on 9/14/17.
+ * Represents a Mood
+ *
+ * @author Ryan De Forest
+ * @version 1.0
+ * @see MoodAngry
+ * @see MoodHappy
+ * @since 1.0
  */
 
 public abstract class Mood {
     private Date date;
 
+    /**
+     * Constructs a Mood object with current time for date
+     */
+
     public Mood(){
         date = new Date();
     }
+
+    /**
+     * Constructs a Mood object with a given date
+     * @param d date
+     */
 
     public Mood(Date d) {
         date = d;
     }
 
+    /**
+     * Returns the date for the Mood object
+     * @return date
+     */
+
     public Date getDate() {
         return date;
     }
 
+
+    /**
+     * Sets the date for the Mood object
+     * @param date date
+     */
+
     public void setDate(Date date) {
         this.date = date;
     }
+
+    /**
+     * Abstract function to return the type of Mood as a string
+     * @return type of Mood
+     */
 
     public abstract String getMood();
 }
