dcca24161cd4047b0ce820c463f63eb7bf572d80
rdeforestuofa <deforest@ualberta.ca>
2017-10-05 19:17:02 -0600

    lab exercise 5

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -2,9 +2,13 @@ package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
+import io.searchbox.annotations.JestId;
+
 public abstract class Tweet implements Tweetable {
     private String message;
     private Date date;
+    @JestId
+    private String id;
 
     public Tweet(String message){
         this.message = message;
@@ -42,5 +46,13 @@ public abstract class Tweet implements Tweetable {
 
     public Date getDate() {
         return date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
